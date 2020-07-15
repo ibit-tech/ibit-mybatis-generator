cdb61de5188f1df271bbee52834a5e5849a1669b
ben <xiaobenma020@gmail.com>
2020-07-15 12:21:50 +0800

    2.1

--- src/test/java/tech/ibit/mybatis/generator/GeneratorTest.java
+++ src/test/java/tech/ibit/mybatis/generator/GeneratorTest.java
@@ -21,8 +21,8 @@ public class GeneratorTest {
 
         // 设置jdbc路径
         String jdbcURL = "jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=convertToNull&useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true";
-        String username = "root";
-        String password = "12345678";
+        String username = "**";
+        String password = "**";
 
         // 指定表明
         String[] tables = new String[]{
